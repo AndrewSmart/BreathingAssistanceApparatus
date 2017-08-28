b92e6b20841146e97b8c02c6fdf88cecdf2e2038
andrew.smart <andrew.smart@aggiemail.usu.edu>
2017-08-28 16:46:50 -0600

    Worked a bit on losses, and flow coefficient. Sometimes the design triangles/products (no losses) are desired, so added a bool for that.

--- analysis/impeller.m
+++ analysis/impeller.m
@@ -10,18 +10,19 @@
 % You should have received a copy of the GNU General Public License along
 % with this program; if not, write to the Free Software Foundation, Inc.,
 % 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 function pressure_flow_curve = impeller(B1_degrees,B2_degrees)
 	% Models characteristics of an impeller with B1_degrees and B2_degrees.
 	format short g;
 	pressure_flow_curve = []
 	#for rpm=3000:400:5400
-	rpm=5400
+	rpm=2427
 		pressure_flow_curve = [pressure_flow_curve; characterize_impeller(rpm,B1_degrees,B2_degrees)];
 	#end
 end
 
 function pressure_flow = tune(B2_degrees)
-	rpm = 5400
+	rpm = 2427
 	B1_degrees=46
 	pressure_target=16
 	#Outer loop b2, inner loop solve for b1 for 4,8,12,16,20 setpoints with .0032 flow
@@ -59,20 +60,19 @@ end
 # pressure=12, then flow won't change as much when adjusting pressure.
 
 function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
+	losses = 0; #Set to 0 if we want ideal design results. 1 to calculate what few losses we do calculate.
 	deg_to_rad_coeff = (pi)/180;
 	g_m_per_s2 = 9.80665;
 	cmH2O_to_pascal_coeff = 98.0665;
 	#--------------------
 	# Pressure setting
-	#disp("---Pressure Setting---");
-	ventilator_pressure_setting_cmH2O = 20;
+	ventilator_pressure_setting_cmH2O = 20
 	ventilator_pressure_setting_Pascals = ventilator_pressure_setting_cmH2O * cmH2O_to_pascal_coeff;
 
 	#--------------------
 	# Density of air calculation
 	# https://en.wikipedia.org/wiki/Density_of_air
 	# Skipped humidity for now
-	#disp("---Density of air calculation---");
 	p0_sea_level_air_pressure_Pascals = 101325;
 	T0_sea_level_standard_temperature_K = 288.15;
 	g_earth_surface_gravitational_acceleration_m_per_s2 = 9.80665;
@@ -83,22 +83,24 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 	h_altitude_meters = 1500;
 	p_pressure_at_altitude_Pascals = p0_sea_level_air_pressure_Pascals * (1-(L_temperature_lapse_rate_K_per_m*h_altitude_meters)/T0_sea_level_standard_temperature_K)^((g_earth_surface_gravitational_acceleration_m_per_s2*M_molar_mass_dry_air_kg_per_mol)/(R_ideal_universal_gas_constant_Joules_per_mol_K*L_temperature_lapse_rate_K_per_m));
 	# Could either get this from sensor reading on startup or UI entry of altitude
-	p_ambient_air_pressure_Pascals = p_pressure_at_altitude_Pascals;
+	p_ambient_air_pressure_Pascals = p_pressure_at_altitude_Pascals
 	temperature_K = 293;
 	R_specific_constant_dry_air_Joules_per_kg_K = 287.058;
-	rho_ambient_air_density_kg_per_m3 = p_ambient_air_pressure_Pascals / (R_specific_constant_dry_air_Joules_per_kg_K*temperature_K);
+	outlet_pressure_Pascals = p_ambient_air_pressure_Pascals + ventilator_pressure_setting_Pascals
+	rho_ambient_air_density_kg_per_m3 = p_ambient_air_pressure_Pascals / (R_specific_constant_dry_air_Joules_per_kg_K*temperature_K)
+	rho_ventilator_air_density_kg_per_m3 = outlet_pressure_Pascals / (R_specific_constant_dry_air_Joules_per_kg_K*temperature_K)
 	#rho_ambient_air_density_kg_per_cm3 = rho_ambient_air_density_kg_per_m3 * 1e-6;
+	C_p_specific_heat_air_kJ_per_kg_K = 1.005; #https://www.ohio.edu/mechanical/thermo/property_tables/air/air_Cp_Cv.html
 
 	# Now calculate pressurized air density
-	rho_ventilator_pressure_air_density_kg_per_m3 = (p_ambient_air_pressure_Pascals + ventilator_pressure_setting_Pascals) / (R_specific_constant_dry_air_Joules_per_kg_K*temperature_K);
+	rho_ventilator_pressure_air_density_kg_per_m3 = outlet_pressure_Pascals / (R_specific_constant_dry_air_Joules_per_kg_K*temperature_K);
 	#rho_ventilator_pressure_air_density_kg_per_cm3 = rho_ventilator_pressure_air_density_kg_per_m3 * 1e-6;
-	#disp(sprintf('Air density kg_per_m3 %f', rho_ventilator_pressure_air_density_kg_per_m3));
+	pressure_ratio = (p_ambient_air_pressure_Pascals+ventilator_pressure_setting_Pascals) / p_ambient_air_pressure_Pascals
 
 	#---------------------
 	# Design variables
 	# angle of blades at inlet, careful about measurement! See figure 1.3 in Principles of Turbomachinery
 	# B1 approaching 90 degrees greatly increases Q and torque
-	#disp("---Design variables---");
 	#B1_degrees = 43.4;
 	B1_rads = B1_degrees * deg_to_rad_coeff;
 	# angle of blades at outlet, careful this is measured opposite u, not inside triangle, see diagram
@@ -119,20 +121,25 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 
 	w_rad_per_min=rpm*2*pi;
 	w_rad_per_sec=w_rad_per_min/60;
-	r1_m = 0.01;
+	r1_m = 0.01; #radius of hub.
 	r2_m = 0.08;
+	dn_m = 2 * r1_m; #diameter of hub.
+	d1a_m = 2 * (r1_m + b1_m); #diameter of inlet.
+	# Turton - "Principles of Turbomachinery" §6.3.2 eqn 6.3-6.4
+	# Z = B2/3 B2 in degrees
+	# Z = 2*pi*sin(B_m)/(.35 to .45)log_e(D2/D1)
+	Z1 = B2_degrees / 3
+	Z2 = 2 * pi * sin((B2_ideal_rads+B1_rads)/2)/(.35*log(r2_m/r1_m))
 	N_Blades = 9;
 
 	#--------------------
 	# Inlet Triangle
 	# inlet impeller tip speed
 	# U is perepheral velocity vector
-	#disp("---Inlet Triangle---");
 	U1_m_per_sec = w_rad_per_sec * r1_m
 	# Impeller tip speed
 	U2_m_per_sec = w_rad_per_sec * r2_m
 	# U2 = pi*D[m]*N[rpm]/60[s]
-	#disp(sprintf('U1 cm_per_sec: %f U2 cm_per_sec: %f', U1_cm_per_sec, U2_cm_per_sec));
 
 	# See Principles of Turbomechanics Exercise 1.1
 	# Urgh in Fluid Mechanics and Turbo Design ch2 replace V with W, and C with V. Cw with Vu, and Cr with Vr
@@ -165,11 +172,27 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 	# Vu = U - W * cos(b)
 	# Wu = W * cos(b) = U - Vu 
 
+	#Lung tidal + reserve volume is 4.8L for males, adult breaths per minute is 12-20.
+	lung_volume_male_liters = 4.8;
+	breaths_max_per_minute = 20;
+	breath_time_seconds = 60 / breaths_max_per_minute;
+	inhalation_time_seconds = breath_time_seconds / 2;
+	inhalation_liters_per_second = lung_volume_male_liters / inhalation_time_seconds;
+	Qtarget_m3_per_sec = inhalation_liters_per_second / 1000
+
+	#---------------------
+	# Flow Coefficient
+	# Swain - "IMPACT OF IMPELLER BLADE TRIMMING ON THE PERFORMANCE OF CENTRIFUGAL COMPRESSORS" §1.2.2,§2.1
+	# Wang et al. - "Design and Performance Evaluation of a Very Low Flow Coefficient Centrifugal Compressor"
+	# TODO: Swain clams it is D2, but Wang et al claims it is D2^2
+	Flow_Coefficient_Swain = 4 * Qtarget_m3_per_sec/(pi*(r2_m*2)*U2_m_per_sec);
+	f_q = 1; #Impeller inlet eyes
+	flow_coefficient_inlet = 4 * Qtarget_m3_per_sec / (f_q * pi * (d1a_m^2-dn_m^2)*U1_m_per_sec)
+
 	#---------------------
 	# Flow calculation
 	#  One book took off the blade area, other two did not. The book also covered many efficiency loss formulas while the other two did not.
 	# Am1 is the area of the impeller at inlet in the [m]eriodional direction
-	#disp("---Flow Calculation---");
 	Am1_m2 = 2 * pi * r1_m * b1_m;
 	# Am2 is the area of the impeller at outlet
 	Am2_m2 = 2 * pi * r2_m * b2_m;
@@ -185,26 +208,30 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 
 	#--------------------
 	# Outlet Triangle
-	#disp("---Outlet Triangle---");
 	# Swain - "IMPACT OF IMPELLER BLADE TRIMMING ON THE PERFORMANCE OF CENTRIFUGAL COMPRESSORS" §1.2.4
-	# Swain shows a popular slip model proposed by Stodola (and alludes to a better jet/wake model):
+	# Swain shows a popular slip model proposed by Stodola (and alludes to a better jet/wake model).
+	# Turton - "Principles of Turbomachinery" §5.3.2 also cites Stodola but the eqn is different...:
 	Vus_m_per_sec = (pi * U2_m_per_sec * cos(B2_ideal_rads)) / N_Blades
 	Vr2_m_per_sec = Q_out_m3_per_sec / Am2_m2
 	Wu2_ideal_m_per_sec = Vr2_m_per_sec * cot(B2_ideal_rads)
 	Vu2_ideal_m_per_sec = U2_m_per_sec - Wu2_ideal_m_per_sec
-	Vu2_m_per_sec = Vu2_ideal_m_per_sec - Vus_m_per_sec
-	Wu2_m_per_sec = U2_m_per_sec - Vu2_m_per_sec
-	# TODO: Derive ideal/actual B2_rads and A2_rads
-	B2_slip_rads = acot(Wu2_m_per_sec/Vr2_m_per_sec);
-	#W2_cm_per_sec = sqrt(U2_cm_per_sec^2+V2_cm_per_sec^2 - 2*U2_cm_per_sec*V2_cm_per_sec * cos(A2_rads))
-	# Simpler W2 eqn 26 from Centrifugal and Rotary Pumps Fundamentals
-	W2_m_per_sec = Vr2_m_per_sec / sin(B2_slip_rads)
+	if (losses==1)
+		Vu2_m_per_sec = Vu2_ideal_m_per_sec - Vus_m_per_sec
+		Wu2_m_per_sec = U2_m_per_sec - Vu2_m_per_sec
+		# TODO: Derive ideal/actual B2_rads and A2_rads
+		B2_slip_rads = acot(Wu2_m_per_sec/Vr2_m_per_sec);
+		#W2_cm_per_sec = sqrt(U2_cm_per_sec^2+V2_cm_per_sec^2 - 2*U2_cm_per_sec*V2_cm_per_sec * cos(A2_rads))
+		# Simpler W2 eqn 26 from Centrifugal and Rotary Pumps Fundamentals
+		W2_m_per_sec = Vr2_m_per_sec / sin(B2_slip_rads)
+	else
+		Vu2_m_per_sec = Vu2_ideal_m_per_sec
+		Wu2_m_per_sec = Wu2_ideal_m_per_sec
+		W2_m_per_sec = Vr2_m_per_sec / sin(B2_ideal_rads)
+	endif
 	A2_rads = atan(Vr2_m_per_sec / Vu2_m_per_sec);
 	A2_degrees = A2_rads / deg_to_rad_coeff
 	#V2_cm_per_sec = sqrt(Vr2_cm_per_sec^2+Vu2_cm_per_sec^2)
 	V2_m_per_sec = Vr2_m_per_sec / sin(A2_rads)
-	#disp(sprintf('V1 cm_per_sec: %f V2 cm_per_sec: %f', V1_cm_per_sec, V2_cm_per_sec))
-	#disp(sprintf('W1 cm_per_sec: %f W2 cm_per_sec: %f', W1_cm_per_sec, W2_cm_per_sec))
 
 	#--------------------
 	# Theoretical Head Calculation
@@ -214,7 +241,6 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 	# Or alternatively
 	#  gH_E = sqrt(V2^2-V1^2)+(u2^2-u1^2)+(W1^2-W2^2))
 	#gH_E = U2_cm_per_sec *Vu2_cm_per_sec-U1_cm_per_sec*Vu1_cm_per_sec
-	#disp("---Head Calculation---");
 	gH_E_m2_per_s2 = U2_m_per_sec * Vu2_m_per_sec - U1_m_per_sec * Vu1_m_per_sec
 	# m^2/s^2 is the same units as J/kg
 	gH_E_J_per_kg = gH_E_m2_per_s2
@@ -226,7 +252,7 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 	delta_pressure_N_per_m2 = delta_pressure_J_per_m3;
 	delta_pressure_Pascals = delta_pressure_J_per_m3;
 	delta_pressure_Psi = delta_pressure_Pascals * 0.000145038;
-	delta_pressure_cmH2O = delta_pressure_Pascals / cmH2O_to_pascal_coeff;
+	delta_pressure_cmH2O = delta_pressure_Pascals / cmH2O_to_pascal_coeff
 
 	#--------------------
 	# Torque/Power Needed
@@ -236,17 +262,8 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 	# Hydrolic Power = rho*Q_volume_flow*gH_E
 	# But for any fluid formula will be
 	# Hydrolic Power = m_dot*gH_E
-	#disp("---Torque/Power needed---");
 	hydrolic_power_J_per_sec = m_dot_mass_flow_rate_kg_per_sec * gH_E_J_per_kg;
 	hydrolic_power_W = hydrolic_power_J_per_sec;
-
-	#Lung tidal + reserve volume is 4.8L for males, adult breaths per minute is 12-20.
-	lung_volume_male_liters = 4.8;
-	breaths_max_per_minute = 20;
-	breath_time_seconds = 60 / breaths_max_per_minute;
-	inhalation_time_seconds = breath_time_seconds / 2;
-	inhalation_liters_per_second = lung_volume_male_liters / inhalation_time_seconds;
-	Qtarget_m3_per_sec = inhalation_liters_per_second / 1000;
 
 	# T_Nm = Power/angular velocity (Fluid Mechanics and Turbo Design)
 	# m_dot is the mass flow rate
@@ -264,20 +281,14 @@ function pressureflow = characterize_impeller(rpm,B1_degrees,B2_degrees)
 
 	#--------------------
 	# Impeller Height Calculation
-	# Swain - "IMPACT OF IMPELLER BLADE TRIMMING ON THE PERFORMANCE OF CENTRIFUGAL COMPRESSORS" §1.2.2,§2.1
-	# Following is for liquid... :-( Need gas formula
-	# TODO: Get gas formula so that no online calculator necessary
-	# TODO: Is the flow coefficient formula given on page 6 the appropriate one?
-	Flow_Factor = (Q_in_m3_per_sec*60*60) * sqrt(1/(delta_pressure_Pascals/1000)); #Metric
-	Flow_Coefficient = Flow_Factor * 1.16 #Imperial
-	Flow_Coefficient_page6 = 4*Q_in_m3_per_sec/(pi*r2_m*2*U2_m_per_sec)
 	# Get z/D2 ratio, solve for z (optimal axial length)
 	# Online calculator lookup is 1.9500cm
-	a_m = (0.084375 + 1.5625 * Flow_Coefficient) * r2_m
+	z_d2 = (0.084375 + 1.5625 * flow_coefficient_inlet)
+	z_m = z_d2 * (2 * r2_m)
 	# TODO! Should Blade length be deduced from B1,B2,r1,r2,and a_m? Or pulled out of CAD model?
 	# For now approximate as triangle at base of blade... seems to be a good approximation but doesn't
 	# consider B1/B2 skew.
-	Blade_length_m = sqrt(a_m^2+(r2_m-r1_m)^2)
+	Blade_length_m = sqrt(z_m^2+(r2_m-r1_m)^2)
 
 	#--------------------
 	# 1D Loss Calculation
